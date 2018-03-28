91fdaa787532efa2abb237fa7e2ad00a9edeac10
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-28 11:50:17 +0200

    Added some helper methods to Price

--- domain/src/main/java/com/switchfully/order/domain/items/prices/Price.java
+++ domain/src/main/java/com/switchfully/order/domain/items/prices/Price.java
@@ -22,6 +22,14 @@ public final class Price {
         return amount.floatValue();
     }
 
+    public static Price add(Price price1, Price price2) {
+        return Price.create(price1.getAmount().add(price2.getAmount()));
+    }
+
+    public boolean sameAs(Price otherPrice) {
+        return amount.equals(otherPrice.getAmount());
+    }
+
     @Override
     public String toString() {
         return "Price{" + "amount=" + amount + '}';
---
+++ domain/src/test/java/com/switchfully/order/domain/items/prices/PriceTest.java
@@ -0,0 +1,28 @@
+package com.switchfully.order.domain.items.prices;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class PriceTest {
+
+    @Test
+    public void add() {
+        Price price1 = Price.create(BigDecimal.valueOf(10));
+        Price price2 = Price.create(BigDecimal.valueOf(10.50));
+
+        Price priceCombined = Price.add(price1, price2);
+
+        Assertions.assertThat(priceCombined.getAmount().equals(BigDecimal.valueOf(20.50)));
+    }
+
+    @Test
+    public void sameAs() {
+        Price price1 = Price.create(BigDecimal.valueOf(10));
+        Price price2 = Price.create(BigDecimal.valueOf(10));
+
+        Assertions.assertThat(price1.sameAs(price2)).isTrue();
+    }
+
+}
